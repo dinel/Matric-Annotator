2fed8e9ecbb929986880ef388b146f01516472fb
Constantin Orasan <dinel@dinel.org.uk>
2020-12-02 13:33:42 +0000

    First commit

---
+++ src/Controller/FrontPageController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FrontPageController extends AbstractController
+{
+    /**
+     * @Route("/", name="front_page")
+     */
+    public function index(): Response
+    {
+        return $this->render('front_page/index.html.twig', [
+            'controller_name' => 'FrontPageController',
+        ]);
+    }
+}
