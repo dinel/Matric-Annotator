860ac26e7c3819b011708338ca50c860e147530a
Constantin Orasan <dinel@dinel.org.uk>
2020-12-30 00:37:32 +0000

    Displays the tasks assigned to a user

--- src/Controller/FrontPageController.php
+++ src/Controller/FrontPageController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Task4User;
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,8 +19,31 @@ class FrontPageController extends AbstractController
             return $this->redirectToRoute('app_login');
         }
 
+        // check whether the user is still valid (probably completely unnecessary)
+        if(! $this->isUserValid($this->getUser())) {
+            return $this->redirectToRoute('app_logout');
+        }
+
+        // get the tasks for this user
+        $entityManager = $this->getDoctrine()->getManager();
+        $tasks = $entityManager->getRepository(Task4User::class)
+            ->findBy(['user' => $this->getUser()]);
+
         return $this->render('front_page/index.html.twig', [
-            'controller_name' => 'FrontPageController',
+            'user' => $this->getUser(),
+            'tasks' => $tasks,
         ]);
+    }
+
+    private function isUserValid(User $user): bool
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $userDatabase = $entityManager->getRepository(User::class)
+                ->find($user->getId());
+        if($userDatabase) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
