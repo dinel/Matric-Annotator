615dd23338f327dca385cc63e77a244ffb4c1d29
Constantin Orasan <dinel@dinel.org.uk>
2021-01-02 19:39:54 +0000

    Implemented basic functionality

--- src/Controller/AnnotationController.php
+++ src/Controller/AnnotationController.php
@@ -2,19 +2,52 @@
 
 namespace App\Controller;
 
+use App\Entity\SegmentPair;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class AnnotationController extends AbstractController
 {
     /**
-     * @Route("/annotation", name="annotation")
+     * @Route("/annotation/{id}", name="annotation", requirements={"id"="\d+"})
      */
-    public function index(): Response
+    public function index(int $id): Response
     {
+        $entityManager = $this->getDoctrine()->getManager();
+        $segment = $entityManager->getRepository(SegmentPair::class)
+                                 ->find($id);
+        $task = $segment->getTask();
+
+        $session = new Session();
+        $session->start();
+        $position = $session->get("position", "right");
+
         return $this->render('annotation/index.html.twig', [
-            'controller_name' => 'AnnotationController',
+            'segment' => $segment,
+            'task' => $task,
+            'position' => $position,
         ]);
+    }
+
+    /**
+     * @Route("/annotation/move-preview")
+     */
+    public function movePreviewAction(): Response
+    {
+        $session = new Session();
+        $session->start();
+
+        $position = $session->get("position", "right");
+        if($position === "right") {
+            $session->set("position", "left");
+        } else {
+            $session->set("position", "right");
+        }
+
+        return new JsonResponse(true);
     }
 }
